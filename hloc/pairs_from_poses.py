d7a8a30c38109e192b3b246c9e942a3898877e9c
jingwen <11710414@mail.sustech.edu.cn>
2024-05-07 19:18:49 +0800

    read text model

--- hloc/pairs_from_poses.py
+++ hloc/pairs_from_poses.py
@@ -4,7 +4,7 @@ import numpy as np
 import scipy.spatial
 
 from . import logger
-from .utils.read_write_model import read_images_binary
+from .utils.read_write_model import read_images_binary, read_images_text
 from .pairs_from_retrieval import pairs_from_score_matrix
 
 DEFAULT_ROT_THRESH = 30  # in degrees
@@ -39,9 +39,14 @@ def get_pairwise_distances(images):
     return ids, dist, dR
 
 
-def main(model, output, num_matched, rotation_threshold=DEFAULT_ROT_THRESH):
+def main(model, output, num_matched, rotation_threshold=DEFAULT_ROT_THRESH, type='txt'):
     logger.info('Reading the COLMAP model...')
-    images = read_images_binary(model / 'images.bin')
+    if type == 'txt':
+        logger.info('Reading the COLMAP model in txt format...')
+        images = read_images_text(model / 'images.txt')
+    else:
+        logger.info('Reading the COLMAP model in binary format...')
+        images = read_images_binary(model / 'images.bin')
 
     logger.info(
         f'Obtaining pairwise distances between {len(images)} images...')
@@ -65,5 +70,6 @@ if __name__ == "__main__":
     parser.add_argument('--num_matched', required=True, type=int)
     parser.add_argument('--rotation_threshold',
                         default=DEFAULT_ROT_THRESH, type=float)
+    parser.add_argument('--type', default='txt', type=str)
     args = parser.parse_args()
     main(**args.__dict__)
