9c82018e99f56a82e1965bb50999feaae5f1b52b
jingwen <jyubt@connect.ust.hk>
2024-05-21 07:46:43 -0400

    bug fix

--- gvbench_utils.py
+++ gvbench_utils.py
@@ -7,11 +7,8 @@ def parser():
       parser.add_argument('--image_path', type=Path)
       parser.add_argument('--output_path', type=Path)
       parser.add_argument('--features', type=Path)
-      
-      
-      parser.add_argument('--feature', type=str)
       parser.add_argument('--pairs', type=Path)
-      parser.add_argument('--features_ref', type=Path)
+      
       parser.add_argument('--matching', action='store_true')
       parser.add_argument('--extraction', action='store_true')
       parser.add_argument('--matching_loftr', action='store_true')
@@ -30,21 +27,21 @@ if __name__ == "__main__":
       
       if args.matching:
             ### Match feature of sequences
-            features = ['superpoint_max', 'sift', 'disk', 'loftr']
+            features = ['superpoint_max', 'sift', 'disk']
             
             for feature in features:
                   if feature == 'superpoint_max':
-                        match_features.main(match_features.confs['superglue'], pairs = Path(args.pairs), features= Path(args.features, f'{features}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_superglue.h5'))
-                        match_features.main(match_features.confs['NN-superpoint'], pairs = Path(args.pairs), features= Path(args.features, f'{features}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_NN.h5'))
+                        match_features.main(match_features.confs['superglue'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_superglue.h5'))
+                        match_features.main(match_features.confs['NN-superpoint'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_NN.h5'))
                   if feature == 'disk':
-                        match_features.main(match_features.confs['disk+lightglue'], pairs = Path(args.pairs), features= Path(args.features, f'{features}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_lightglue.h5'))
-                        match_features.main(match_features.confs['NN-ratio'], pairs = Path(args.pairs), features= Path(args.features, f'{features}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_NN.h5'))
+                        match_features.main(match_features.confs['disk+lightglue'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_lightglue.h5'))
+                        match_features.main(match_features.confs['NN-ratio'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_NN.h5'))
                   if feature == 'sift':
-                        match_features.main(match_features.confs['NN-ratio'], pairs = Path(args.pairs), features= Path(args.features, f'{features}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_NN.h5'))
+                        match_features.main(match_features.confs['NN-ratio'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_NN.h5'))
       
       if args.matching_loftr:
             ### Match LoFTR of sequences
-            match_dense.main(match_dense.confs['loftr'], Path(args.pairs), image_dir= Path(args.image_path), matches= Path(args.output_path, f'{Path(args.pairs).stem}_loftr.h5'), features= Path(args.features, f'{Path(args.pairs).stem}_loftr_kpts.h5'))
+            match_dense.main(match_dense.confs['loftr'], pairs = Path(args.pairs), image_dir= Path(args.image_path), matches= Path(args.output_path, f'{Path(args.pairs).stem}_loftr.h5'), features= Path(args.features, f'{Path(args.pairs).stem}_loftr_kpts.h5'))
       
       # conf = extract_features.confs[args.feature]
       # extract_features.main(conf, Path(args.image_path), feature_path = Path(args.output_path))
