b7000908aabe1137859144ae2040ed48f8896d28
jingwen <jyubt@connect.ust.hk>
2024-05-22 04:48:15 -0400

    bug fix

--- gvbench_utils.sh
+++ gvbench_utils.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 # First extract features
-echo "Extracting features..."
-python gvbench_utils.py --extraction --image_path datasets/GV-Bench/release/images --output_path datasets/GV-Bench/release/features
+# echo "Extracting features..."
+# python gvbench_utils.py --extraction --image_path datasets/GV-Bench/release/images --output_path datasets/GV-Bench/release/features
 
 # Then match the features
 # day sequence
-echo "Matching features for day sequence..."
-python gvbench_utils.py --matching --pairs datasets/GV-Bench/release/gt/day.txt --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
+# echo "Matching features for day sequence..."
+# python gvbench_utils.py --matching --pairs datasets/GV-Bench/release/gt/day.txt --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
 
-# night sequence
-echo "Matching features for night sequence..."
-python gvbench_utils.py --matching --pairs datasets/GV-Bench/release/gt/night.txt --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
+# # night sequence
+# echo "Matching features for night sequence..."
+# python gvbench_utils.py --matching --pairs datasets/GV-Bench/release/gt/night.txt --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
 
-# season sequence
-echo "Matching features for season sequence..."
-python gvbench_utils.py --matching --pairs datasets/GV-Bench/release/gt/season.txt --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
+# # season sequence
+# echo "Matching features for season sequence..."
+# python gvbench_utils.py --matching --pairs datasets/GV-Bench/release/gt/season.txt --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
 
-# weather sequence
-echo "Matching features for weather sequence..."
-python gvbench_utils.py --matching --pairs datasets/GV-Bench/release/gt/weather.txt --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
+# # weather sequence
+# echo "Matching features for weather sequence..."
+# python gvbench_utils.py --matching --pairs datasets/GV-Bench/release/gt/weather.txt --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
 
 # Match dense using LoFTR
 echo "Matching featurs by LoFTR..."
 
 echo "Matching features for day sequence.."
-python gvbench_utils.py --matching_loftr --pairs datasets/GV-Bench/release/gt/day.txt --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
+python gvbench_utils.py --matching_loftr --pairs datasets/GV-Bench/release/gt/day.txt --image_path datasets/GV-Bench/release/images --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
 
 echo "Matching features for night sequence.."
-python gvbench_utils.py --matching_loftr --pairs datasets/GV-Bench/release/gt/night.txt --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
+python gvbench_utils.py --matching_loftr --pairs datasets/GV-Bench/release/gt/night.txt --image_path datasets/GV-Bench/release/images --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
 
 echo "Matching features for season sequence.."
-python gvbench_utils.py --matching_loftr --pairs datasets/GV-Bench/release/gt/season.txt --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
+python gvbench_utils.py --matching_loftr --pairs datasets/GV-Bench/release/gt/season.txt --image_path datasets/GV-Bench/release/images --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
 
 echo "Matching features for weather sequence.."
-python gvbench_utils.py --matching_loftr --pairs datasets/GV-Bench/release/gt/weather.txt --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
+python gvbench_utils.py --matching_loftr --pairs datasets/GV-Bench/release/gt/weather.txt --image_path datasets/GV-Bench/release/images --features datasets/GV-Bench/release/features --output_path datasets/GV-Bench/release/matches
