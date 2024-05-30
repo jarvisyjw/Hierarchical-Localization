d6edc3fbd3861bd8da7fe5f50151558aaba28900
jingwen <11710414@mail.sustech.edu.cn>
2024-05-30 18:22:59 +0800

    bug fix

--- gvbench_utils.py
+++ gvbench_utils.py
@@ -8,6 +8,8 @@ def parser():
       parser.add_argument('--output_path', type=Path)
       parser.add_argument('--features', type=Path)
       parser.add_argument('--pairs', type=Path)
+      parser.add_argument('--feature', type=str)
+      parser.add_argument('--matcher', type=str)
       
       parser.add_argument('--matching', action='store_true')
       parser.add_argument('--extraction', action='store_true')
@@ -20,24 +22,43 @@ if __name__ == "__main__":
       args = parser()
       if args.extraction:
             ### Extract features of each sequences
-            features = ['superpoint_max', 'sift', 'disk']
+            if args.feature is not None:
+                  features = [args.feature]
+            else:
+                  features = ['superpoint_max', 'sift', 'disk']
             for feature in features:
                   conf = extract_features.confs[feature]
                   extract_features.main(conf, Path(args.image_path), feature_path = Path(args.output_path, f'{feature}.h5'))
       
       if args.matching:
             ### Match feature of sequences
-            features = ['superpoint_max', 'sift', 'disk']
+            if args.feature is not None:
+                  features = [args.feature]
+                  if args.matcher is not None:
+                        matchers = [args.matcher]
+                  
+                  for feature in features:
+                        if feature == 'superpoint_max':
+                              for matcher in matchers:
+                                    match_features.main(match_features.confs[matcher], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_{matcher}.h5'))
+                        if feature == 'disk':
+                              for matcher in matchers:
+                                    match_features.main(match_features.confs[matcher], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_{matcher}.h5'))
+                        if feature == 'sift':
+                              match_features.main(match_features.confs['NN-ratio'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_NN.h5'))
+                  
+            else:
+                  features = ['superpoint_max', 'sift', 'disk']
             
-            for feature in features:
-                  if feature == 'superpoint_max':
-                        match_features.main(match_features.confs['superglue'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_superglue.h5'))
-                        match_features.main(match_features.confs['NN-superpoint'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_NN.h5'))
-                  if feature == 'disk':
-                        match_features.main(match_features.confs['disk+lightglue'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_lightglue.h5'))
-                        match_features.main(match_features.confs['NN-ratio'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_NN.h5'))
-                  if feature == 'sift':
-                        match_features.main(match_features.confs['NN-ratio'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_NN.h5'))
+                  for feature in features:
+                        if feature == 'superpoint_max':
+                              match_features.main(match_features.confs['superglue'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_superglue.h5'))
+                              match_features.main(match_features.confs['NN-superpoint'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_NN.h5'))
+                        if feature == 'disk':
+                              match_features.main(match_features.confs['disk+lightglue'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_lightglue.h5'))
+                              match_features.main(match_features.confs['NN-ratio'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_NN.h5'))
+                        if feature == 'sift':
+                              match_features.main(match_features.confs['NN-ratio'], pairs = Path(args.pairs), features= Path(args.features, f'{feature}.h5'), matches= Path(args.output_path, f'{Path(args.pairs).stem}_{feature}_NN.h5'))
       
       if args.matching_loftr:
             ### Match LoFTR of sequences
